a83d1d6cde4141f053be68249f6ce0c17deb6dac
Hattinger04 <s.8greiderer@gmail.com>
2022-05-02 22:50:05 +0200

    init

---
+++ src/components/Header.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="container">
+        <h1>{{ msg }}</h1>
+        <Button/>
+    </div>
+</template>
+
+
+<script>
+import Button from './Button.vue'
+    export default {
+        name: 'Header',
+        data() {
+            return {
+                hello : 'Hallo'
+            }
+        },
+        props: {
+            msg : {
+                type: String,
+                default : 'Hallo Welt'
+            }
+        }, 
+        components : {
+            Button
+        }
+    }
+</script>
+
+<style scoped>
+    div{
+        height: 100%;
+    }
+</style>
